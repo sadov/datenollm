69e488dc980bbceda0a1f51bd75d833229c52854
Oleg Sadov <oleg.sadov@gmail.com>
2025-08-04 00:34:17 +0300

    Dateno part changed
    Signed-off-by: Oleg Sadov <oleg.sadov@gmail.com>

--- datenollm/dateno.py
+++ datenollm/dateno.py
@@ -1,12 +1,13 @@
 import os
+import logging
 import dateno.core
 
 DATENO_API_KEY = os.getenv('DATENO_API_KEY')
 
 # Dateno search machinery
-def index_search(query, filters, offset=0, page=1, limit=500):
+def dateno_index_search(query, filters, offset=0, page=1, limit=500):
     "Call Dateno API for search in index"
-    print(f'index_search {query=} {filters=}')
+    logging.debug(f'index_search {query=} {filters=}')
 
     cmd=dateno.core.DatenoCmd(debug=True,
                               apikey=DATENO_API_KEY)
@@ -18,3 +19,17 @@ def index_search(query, filters, offset=0, page=1, limit=500):
                              limit=limit
                              )
     return results
+
+def llm_index_search(llm_response):
+    queries = []
+    for query in llm_response['queries']:
+      if query['filters']:
+        qfilters = [f'{f["name"]}={f["value"]}' for f in query['filters']]
+      else:
+        qfilters = []
+      logging.debug(f'{query=} {qfilters=}')
+      results = dateno_index_search(query['query'], qfilters)#, offset, page, limit)
+      queries.append({'query': query,
+                      'results': results})
+
+    return queries
